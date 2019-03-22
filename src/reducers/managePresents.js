0bc71161d3b2eaa1c1657dd890a496947a478ea1
SamuelC28 <samuel@noukod.com>
2019-03-22 13:51:34 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
+
 export function managePresents(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1 }
+    default:
+      return state;
+  }
 }
+
