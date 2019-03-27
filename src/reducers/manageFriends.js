672fa7ff4d687609f77a1b8264f5ff1a9d36a1c2
joecleverman <josuea@noukod.com>
2019-03-27 14:12:46 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
-}
+    switch (action.type) {
+      case "ADD_FRIEND":
+       return (
+         {...state,
+           friends: [
+             ...state.friends,
+             action.friend
+           ]
+         });
+       case "REMOVE_FRIEND":
+          return (
+             {...state,
+               friends: state.friends.filter(friend => friend.id !== action.id)}
+          );
+      default:
+       return state
+    }
+ 
+ 
+ }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,12 @@
+
 export function managePresents(state, action){
-}
+    switch (action.type) {
+      case "INCREASE":
+        return {numberOfPresents: state.numberOfPresents + 1};
+      case "DECREASE":
+        return {numberOfPresents: state.numberOfPresents - 1};
+      default:
+        return state;
+  
+    }
+  }
