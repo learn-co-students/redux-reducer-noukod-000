c113f5934814985569abf872cb96c33d9dd8e119
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-24 15:20:51 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+   switch (action.type) {
+    case 'ADD_FRIEND':
+      return {friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      const index = state.friends.findIndex(friend => friend.id === action.id)
+      return {friends: [...state.friends.slice(0, index), ...state.friends.slice(index+1)]}
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+  switch(action.type){
+        case "INCREASE":
+            return { ...state, numberOfPresents: state.numberOfPresents + 1 }
+        default:
+            return state
+}
 }
