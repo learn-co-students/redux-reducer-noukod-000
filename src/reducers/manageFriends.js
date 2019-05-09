c38936b280ab0a88d9b2d43fb12132fb9a00d267
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-09 17:37:20 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+  case 'ADD_FRIEND':
+    return {...state, friends: [...state.friends, action.friend]}
+  case 'REMOVE_FRIEND':
+    return {...state, friends: state.friends.filter(friend => friend.id !== action.id)}
+  default:
+    return state;
+  }
 }
