06979e9b79b4affd3980dc9e1016f4760b3533e8
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-10-17 18:10:34 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,5 +1,34 @@
 export function manageFriends(state, action){
   switch(action.type) {
-    
+    case "ADD_FRIEND":
+      return {
+        friends: [...state.friends, action.friends]
+      }
+      
+    case 'REMOVE_FRIEND':
+      let removeFriendId = action.id
+      let removeFriend = state.friends.filter(function(f){return f.id !== removeFriendId})
+      return {friends: removeFriend}
+       default:
+         return state;
+        
   }
 }
+
+
+
+// case 'ADD_FRIEND':
+//       return {
+//         ...state,
+//         friends: [...state.friends, action.friend]
+//       }
+//     case 'REMOVE_FRIEND':
+//       const removedFriend = state.friends.findIndex(friend => friend.id === action.id)
+//       return {
+//         ...state,
+//         friends: [...state.friends.slice(0, removedFriend), //captures first part of array up to removedFriend
+//         ...state.friends.slice(removedFriend + 1)] //captures rest of state.friends array after removedFriend index
+//       }
+//     default:
+//       return state;
+//   }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,7 @@
 export function managePresents(state, action){
+  if (action.type === "INCREASE"){
+    return {numberOfPresents: state.numberOfPresents + 1}
+  } else {
+    return state
+  }
 }
