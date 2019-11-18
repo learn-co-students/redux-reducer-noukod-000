1e2293a1d294817c071c1882ca744ad2cd426279
Ricartho Franck <ricartho.franck@esih.edu>
2019-11-18 03:16:13 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
+let state = {friends: []}
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      return(
+              {...state,
+                friends:[...state.friends,action.friend]
+              }
+            )
+    case 'REMOVE_FRIEND':
+        const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+        return (
+          {...state,
+              friends: [
+                ...state.friends.slice(0, removalIndex),
+                ...state.friends.slice(removalIndex + 1)
+              ]
+          }
+        )
+
+    default:
+      return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,12 @@
+let state = {numberOfPresents: 0}
+
 export function managePresents(state, action){
+    switch(action.type){
+      case 'INCREASE':
+        return (
+                  {...state,numberOfPresents: state.numberOfPresents + 1}
+               )
+      default:
+        return state
+    }
 }
