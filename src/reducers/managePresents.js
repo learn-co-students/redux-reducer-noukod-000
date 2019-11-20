7d83bb5391975bc73d98bd072fd984a8bcd550b2
POLIARD DJimmy <poliarddjimmy@gmail.com>
2019-11-20 09:31:56 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
+let state = {numberOfPresents: 0}
+let action = {type: 'INCREASE'}
+
 export function managePresents(state, action){
-}
+    if (action.type === "INCREASE"){
+      return {numberOfPresents: state.numberOfPresents + 1}
+    } else {
+      return state
+    }
+  }
